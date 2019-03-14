6c3f545df51b06a3cbb863609baa2121912f9c94
selvak <sonytv710@gmail.com>
2019-03-14 07:23:35 +0530

    chnage port

--- src/components/common/Config.js
+++ src/components/common/Config.js
@@ -1,4 +1,4 @@
-export const SERVICE_URL = "http://localhost:8080/api/";
+export const SERVICE_URL = "http://localhost:8081/api/";
 export const USER_SERVICE_URL = `${SERVICE_URL}user/`;
 export const PROJECT_SERVICE_URL = `${SERVICE_URL}project/`;
 export const PARENT_TASK_SERVICE_URL = `${SERVICE_URL}parenttask/`;
